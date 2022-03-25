87beb2ad434c82b30a06105c886bf707f730faeb
NathanM <nathan.milburn@outlook.com>
2022-03-24 18:55:04 -0700

    Added in boilerplate code for backend server and routes

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+// const routes = require('./controllers');
+const path = require('path');
+require('dotenv').config();
+
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const hbs = exphbs.create({});
+
+const sess = {
+  secret: process.env.SESS_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening!'));
+});
