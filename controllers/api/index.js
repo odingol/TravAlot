90520ec0cd0f1d7a19f2886d6c0ffdb1e2653730
NathanM <nathan.milburn@outlook.com>
2022-03-28 20:13:42 -0700

    reformatted handlebars files and created some routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,15 @@ const router = require('express').Router();
 
 // End points
 const userRoutes = require('./usersRoutes');
+const resortRoutes = require('./resortRoutes');
+const waterParkRoutes = require('./waterParkRoutes');
+const themeParkRoutes = require('./themeParkRoutes');
+const rollerCoasterRoutes = require('./rollerCoasterRoutes');
 
 router.use('/users', userRoutes);
+router.use('/resorts', resortRoutes);
+router.use('/waterparks', waterParkRoutes);
+router.use('/themeparks', themeParkRoutes);
+router.use('/rollercoasters', rollerCoasterRoutes);
 
 module.exports = router;
