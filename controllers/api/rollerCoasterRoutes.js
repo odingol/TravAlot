dd5e7191378464f94ecf612b6c1e4e32cf1d1f8d
laperolanegra <laperolanegra@gmail.com>
2022-03-28 21:47:40 -0700

    configured waterpark and theme park

--- controllers/api/rollerCoasterRoutes.js
+++ controllers/api/rollerCoasterRoutes.js
@@ -1,3 +1,14 @@
 const router = require('express').Router();
+const {rollerCoaster} = require('../../models');
+
+router.get('/', async (req,res ) => {
+    try{
+        const coasterData = await rollerCoaster.findAll()
+        const rollercoasters = coasterData.map((index) => index.get({ plain: true }));
+        res.status(200).json(rollercoasters);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
