b959f1d97d9d254b252d057d5ab683bb85ba98cc
NathanM <nathan.milburn@outlook.com>
2022-04-01 19:18:15 -0700

    test heroku

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 // const mysql = require('mysql2');
 require('dotenv').config();
 
+// heroku test
 // Sequelize Method
 const sequelize = process.env.JAWSDB_URL ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
