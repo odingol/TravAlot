40fda9146e8d2bea2baa1465c18f5e97998d4375
Lamor Odingo <l2odingo@gmail.com>
2022-04-04 17:24:08 -0700

    adding starter code for event listeners

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+// save attraction functionality
+// get all save buttons save-attraction-btn
+// add click event listener
+//      - pull data-attraction-type and data-id attributes 
+//  make api post call to save selectiont to db (endpoint needs to be added)
